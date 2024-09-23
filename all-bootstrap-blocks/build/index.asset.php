f1412289fc775f46594040923825058d46c8bf5d
Sean Murphy <sean@iamseanmurphy.com>
2024-09-23 15:47:44 +0000

    1.3.23

---
+++ all-bootstrap-blocks/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-keycodes', 'wp-notices', 'wp-polyfill', 'wp-primitives', 'wp-url', 'wp-viewport'), 'version' => 'bba08488d4ac95261ee90722c041bca6');
